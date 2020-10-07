3cd0d55721c6b46750ca06d63d05d26a0c3b3200
VishalAnand07 <40723537+VishalAnand07@users.noreply.github.com>
2020-10-07 11:53:33 +0530

    adding Lecture 20

---
+++ src/com/company/Lecture20/Main.java
@@ -0,0 +1,30 @@
+package com.company.Lecture20;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomHashTable<Integer, Integer> dict = new CustomHashTable<>();
+
+        for (int i = 0; i < 1000000; i++) {
+            dict.put(i, i*2);
+        }
+
+//        dict.put("apple", "a sweet red fruit");
+//        dict.put("moti", "king of fruits");
+//
+//        System.out.println(dict.get("apple"));
+//
+//        dict.put("apple", "a sour red fruit");
+//
+//        System.out.println(dict.get("apple"));
+//
+//        dict.remove("apple");
+//
+//        System.out.println(dict.get("lichi"));
+//
+//        System.out.println("meaning".hashCode() % 20);
+
+    }
+}
---
+++ src/com/company/Lecture20/SetClient.java
@@ -0,0 +1,11 @@
+package com.company.Lecture20;
+
+
+import java.util.HashSet;
+
+public class SetClient {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+
+    }
+}
